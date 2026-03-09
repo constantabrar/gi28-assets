d9874b58a9358ed9458d519600225bf11561b43e
constantabrar <const.abrar@proton.me>
2026-03-09 15:02:19 +0530

    Create images.js

---
+++ 2nd/images.js
@@ -0,0 +1,80 @@
+const fallbackData = [
+  {
+    id: "01",
+    title: "Gallery 1",
+    image: "https://constantabrar.github.io/gi28-assets/abrar.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/abrar.png"
+  },
+  {
+    id: "02",
+    title: "Gallery 2",
+    image: "https://constantabrar.github.io/gi28-assets/mariya.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/mariya.png"
+  },
+  {
+    id: "03",
+    title: "Gallery 3",
+    image: "https://constantabrar.github.io/gi28-assets/p3.0.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p3.0.png"
+  },
+  {
+    id: "04",
+    title: "Gallery 4",
+    image: "https://constantabrar.github.io/gi28-assets/p3.1.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p3.1.png"
+  },
+  {
+    id: "05",
+    title: "Gallery 5",
+    image: "https://constantabrar.github.io/gi28-assets/p3.2.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p3.2.png"
+  },
+  {
+    id: "06",
+    title: "Gallery 6",
+    image: "https://constantabrar.github.io/gi28-assets/p4.0.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p4.0.png"
+  },
+  {
+    id: "07",
+    title: "Gallery 7",
+    image: "https://constantabrar.github.io/gi28-assets/p4.1.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p4.1.png"
+  },
+  {
+    id: "08",
+    title: "Gallery 8",
+    image: "https://constantabrar.github.io/gi28-assets/p5.0.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p5.0.png"
+  },
+  {
+    id: "09",
+    title: "Gallery 9",
+    image: "https://constantabrar.github.io/gi28-assets/p5.1.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p5.1.png"
+  },
+  {
+    id: "10",
+    title: "Gallery 10",
+    image: "https://constantabrar.github.io/gi28-assets/p6.0.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p6.0.png"
+  },
+  {
+    id: "11",
+    title: "Gallery 11",
+    image: "https://constantabrar.github.io/gi28-assets/p7.0.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p7.0.png"
+  },
+  {
+    id: "12",
+    title: "Gallery 12",
+    image: "https://constantabrar.github.io/gi28-assets/p1.0.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p1.0.png"
+  },
+  {
+    id: "13",
+    title: "Gallery 13",
+    image: "https://constantabrar.github.io/gi28-assets/p8.0.png",
+    highRes: "https://constantabrar.github.io/gi28-assets/p8.0.png"
+  }
+];
